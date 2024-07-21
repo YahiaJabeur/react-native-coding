c6df80c212591fa0cc37fec48833b76fea1ebab8
YahiaJabeur <yahiajabeur@gmail.com>
2024-07-21 21:16:14 +0200

    refactor: update User type

--- types/User.ts
+++ types/User.ts
@@ -1,10 +1,10 @@
 export interface User {
   id: string;
-  first_name: string;
-  last_name: string;
-  text: string;
-  email: string;
-  backgroundColor: string;
-  avatar: string;
-  avatar_large: string;
+  first_name?: string;
+  last_name?: string;
+  text?: string;
+  email?: string;
+  backgroundColor?: string;
+  avatar?: string;
+  avatar_large?: string;
 }
